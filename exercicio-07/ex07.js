7fb274133f0829becf1dd4ddaf29ab3ce58e2020
Enzo Lanzi <enzolanzi189@gmail.com>
2025-03-31 21:39:46 -0300

    exercicio 07

---
+++ exercicio-07/ex07.js
@@ -0,0 +1,5 @@
+let entrada = parseInt(prompt("Digite uma quantidade de meses para saber a soma dos seus dias: "))
+
+let resultado = entrada * 30
+
+alert("A soma dos dias de " + entrada + " meses  é " + resultado)
