2890920277862a93fc441fa7b604038fcfccbf2e
Enzo Lanzi <enzolanzi189@gmail.com>
2025-03-31 21:21:25 -0300

    exercicio 04

---
+++ exercicio-04/ex04.js
@@ -0,0 +1,8 @@
+let num1 = parseFloat(prompt("Digite um número para saber a média: "))
+let num2 = parseFloat(prompt("Digite outro número: "))
+let num3 = parseFloat(prompt("Digite outro número: "))
+
+soma = num1 + num2 + num3
+media = soma / 3
+
+alert("A média dos três números fornecidos é: " + media)
