b19a31e077522147b4873affb5d69db92e142c8d
Enzo Lanzi <enzolanzi189@gmail.com>
2025-03-31 21:26:57 -0300

    exercicio 05

---
+++ exercicio-05/ex05.js
@@ -0,0 +1,5 @@
+let temperatura = parseFloat(prompt("Digite a temperatura em ºC para converte-la para fahrenheit"))
+
+let fahrenheit = 1.8*temperatura + 32
+
+alert(temperatura +" ºC covertido para fahrenheit é: " + fahrenheit + " °F")
