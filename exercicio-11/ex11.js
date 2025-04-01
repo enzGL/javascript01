e3ea198cd817ad9afdc197a7c3e031087c684ce0
Enzo Lanzi <enzolanzi189@gmail.com>
2025-03-31 22:01:38 -0300

    exercicio 11

---
+++ exercicio-11/ex11.js
@@ -0,0 +1,13 @@
+let nota1 = parseFloat(prompt("Digite a 1 nota: "))
+let nota2 = parseFloat(prompt("Digite a 2 nota: "))
+let nota3 = parseFloat(prompt("Digite a 3 nota: "))
+let nota4 = parseFloat(prompt("Digite a 4 nota: "))
+
+let resultado = (nota1 + nota2 + nota3 + nota4) / 4
+
+if(resultado > 4){
+    alert("O aluno foi aprovado")
+}
+else if (resultado < 5){
+    alert("O aluno foi reprovado")
+}
