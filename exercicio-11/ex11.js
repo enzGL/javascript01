4d3dfbdece0c0de6d36eec976724e2657809ebc9
Enzo Lanzi <enzolanzi189@gmail.com>
2025-03-31 22:07:54 -0300

    correção ex 11

--- exercicio-11/ex11.js
+++ exercicio-11/ex11.js
@@ -1,3 +1,4 @@
+let nome = prompt("Digite o nome do aluno: ")
 let nota1 = parseFloat(prompt("Digite a 1 nota: "))
 let nota2 = parseFloat(prompt("Digite a 2 nota: "))
 let nota3 = parseFloat(prompt("Digite a 3 nota: "))
@@ -6,8 +7,8 @@ let nota4 = parseFloat(prompt("Digite a 4 nota: "))
 let resultado = (nota1 + nota2 + nota3 + nota4) / 4
 
 if(resultado > 4){
-    alert("O aluno foi aprovado")
+    alert("O aluno" + nome + " foi aprovado")
 }
 else if (resultado < 5){
-    alert("O aluno foi reprovado")
+    alert("O aluno" + nome + "foi reprovado")
 }
