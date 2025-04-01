5b8142ac2060fe06e272b4b1969a3dd818240e86
Enzo Lanzi <enzolanzi189@gmail.com>
2025-03-31 21:43:08 -0300

    exercicio 08

---
+++ exercicio-08/ex08.js
@@ -0,0 +1,10 @@
+let num1 = parseFloat(prompt("Digite um número para saber a média: "))
+let num2 = parseFloat(prompt("Digite outro número: "))
+let num3 = parseFloat(prompt("Digite outro número: "))
+let num4 = parseFloat(prompt("Digite outro número: "))
+let num5 = parseFloat(prompt("Digite outro número: "))
+
+soma = num1 + num2 + num3 + num4 + num5
+media = soma / 5
+
+alert("A média dos números fornecidos é: " + media)
